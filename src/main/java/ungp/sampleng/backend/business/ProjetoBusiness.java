a6301475847a2ae27fa122845ef2c79074200a0c
rafael.andrade <rafael.andrade@softplan.com.br>
2014-12-04 19:25:28 -0200

    Novas correcoes

--- src/main/java/ungp/sampleng/backend/business/ProjetoBusiness.java
+++ src/main/java/ungp/sampleng/backend/business/ProjetoBusiness.java
@@ -27,39 +27,39 @@ import com.microsoft.tfs.core.clients.workitem.project.ProjectCollection;
 @Component
 public class ProjetoBusiness {
 
-	private TFSTeamProjectCollection connection;
+    private TFSTeamProjectCollection connection;
 
-	@PostConstruct
-	private void postConstruct() {
-		connection = TfsConnector.connectToTFS();
-	}
+    @PostConstruct
+    private void postConstruct() {
+        connection = TfsConnector.connectToTFS();
+    }
 
-	public List<ProjetoDTO> findAll() {
-		WorkItemClient workItemClient = connection.getWorkItemClient();
-		VersionControlClient versionControlClient = connection.getVersionControlClient();
-		ItemIdentifier itemIdentifier = new ItemIdentifier("$/FXDNet/fManutencao");
-		BranchObject[] branchObjects = versionControlClient.queryBranchObjects(itemIdentifier, RecursionType.FULL);
-		System.out.println(branchObjects);
-//		Changeset[] pete = versionControlClient.queryHistory("$/FXDNet/fManutencao", LatestVersionSpec.INSTANCE, 0, RecursionType.FULL, "", VersionSpec.parseSingleVersionFromSpec("95000", null),
-//				null, Integer.MAX_VALUE, false, false, false, false);
-//		System.out.println(pete);
+    public List<ProjetoDTO> findAll() {
+        WorkItemClient workItemClient = connection.getWorkItemClient();
+//        VersionControlClient versionControlClient = connection.getVersionControlClient();
+//      ItemIdentifier itemIdentifier = new ItemIdentifier("$/FXDNet/fManutencao");
+//      BranchObject[] branchObjects = versionControlClient.queryBranchObjects(itemIdentifier, RecursionType.FULL);
+//      System.out.println(branchObjects);
+//      Changeset[] pete = versionControlClient.queryHistory("$/FXDNet/fManutencao", LatestVersionSpec.INSTANCE, 0, RecursionType.FULL, "", VersionSpec.parseSingleVersionFromSpec("95000", null),
+//              null, Integer.MAX_VALUE, false, false, false, false);
+//      System.out.println(pete);
 
-		ProjectCollection projects = workItemClient.getProjects();
-		List<ProjetoDTO> projetos = new ArrayList<ProjetoDTO>();
-		for (Project project : projects) {
-			ProjetoDTO projetoDTO = new ProjetoDTO();
-			projetoDTO.setNome(project.getName());
-			projetos.add(projetoDTO);
-		}
+        ProjectCollection projects = workItemClient.getProjects();
+        List<ProjetoDTO> projetos = new ArrayList<ProjetoDTO>();
+        for (Project project : projects) {
+            ProjetoDTO projetoDTO = new ProjetoDTO();
+            projetoDTO.setNome(project.getName());
+            projetos.add(projetoDTO);
+        }
 
-		Collections.sort(projetos, new Comparator<ProjetoDTO>() {
+        Collections.sort(projetos, new Comparator<ProjetoDTO>() {
 
-			@Override
-			public int compare(ProjetoDTO o1, ProjetoDTO o2) {
-				return o1.getNome().compareTo(o2.getNome());
-			}
-		});
+            @Override
+            public int compare(ProjetoDTO o1, ProjetoDTO o2) {
+                return o1.getNome().compareTo(o2.getNome());
+            }
+        });
 
-		return projetos;
-	}
+        return projetos;
+    }
 }
